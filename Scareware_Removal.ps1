f52f52c642a84870385bebe6fe86febf6235c8ae
Preston Callos <preston@jrpcinfosec.com>
2023-12-14 00:23:27 -0700

    first

--- Scareware_Removal.ps1
+++ Scareware_Removal.ps1
@@ -1,20 +1,14 @@
-# Get the path to the Chrome user data directory
-$chromeUserDataDir = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+# Get the Chrome profiles directory
+$chromeProfilesDir = "$env:LOCALAPPDATA\Google\Chrome\User Data"
 
-# Check if the Chrome user data directory exists
-if (Test-Path $chromeUserDataDir) {
-    # Enumerate the subdirectories in the Chrome user data directory
-    $profiles = Get-ChildItem -Path $chromeUserDataDir -Directory
+# List all the profiles
+$profiles = Get-ChildItem -Path $chromeProfilesDir -Directory | Select-Object -ExpandProperty Name
 
-    # Loop through each profile directory
-    foreach ($profile in $profiles) {
-        # Get the profile name from the directory name
-        $profileName = $profile.Name
-
-        # Do something with the profile name
-        Write-Host "Found Chrome profile: $profileName"
+# Loop through each profile and delete the cache
+foreach ($profile in $profiles) {
+    $cacheDir = Join-Path -Path $chromeProfilesDir -ChildPath "$profile\Cache"
+    if (Test-Path -Path $cacheDir) {
+        Remove-Item -Path $cacheDir -Recurse -Force
+        Write-Host "Cache deleted for profile: $profile"
     }
-}
-else {
-    Write-Host "Chrome user data directory not found."
 }

b2ef60c9b5561108b1e593ed7767337d999619de
Preston Callos <preston@jrpcinfosec.com>
2023-12-14 00:25:31 -0700

    first

--- Scareware_Removal.ps1
+++ Scareware_Removal.ps1
@@ -1,14 +1,8 @@
-# Get the Chrome profiles directory
-$chromeProfilesDir = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+$chromeProfilePath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+$profileFolders = Get-ChildItem -Path $chromeProfilePath -Directory
 
-# List all the profiles
-$profiles = Get-ChildItem -Path $chromeProfilesDir -Directory | Select-Object -ExpandProperty Name
-
-# Loop through each profile and delete the cache
-foreach ($profile in $profiles) {
-    $cacheDir = Join-Path -Path $chromeProfilesDir -ChildPath "$profile\Cache"
-    if (Test-Path -Path $cacheDir) {
-        Remove-Item -Path $cacheDir -Recurse -Force
-        Write-Host "Cache deleted for profile: $profile"
-    }
+$profileNames = $profileFolders | ForEach-Object {
+    $_.Name
 }
+
+$profileNames

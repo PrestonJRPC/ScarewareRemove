cac325321011886153df2f9a672d4d15c2ab65f4
Preston Callos <preston@jrpcinfosec.com>
2023-12-14 00:37:08 -0700

    browserhistory

--- Scareware_Removal.ps1
+++ Scareware_Removal.ps1
@@ -1,15 +1,30 @@
-$chromeProfilesPath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
-
-# Get all Chrome profile directories
-$profileDirectories = Get-ChildItem -Path $chromeProfilesPath -Directory -Filter "Profile*"
-
-foreach ($profileDirectory in $profileDirectories) {
-    $cachePath = Join-Path -Path $profileDirectory.FullName -ChildPath "Cache"
+function Get-ChromeHistory {
+    $historyFile = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History"
     
-    if (Test-Path -Path $cachePath) {
-        $cacheSize = (Get-ChildItem -Path $cachePath -Recurse | Measure-Object -Property Length -Sum).Sum
-        $cacheSizeInMB = $cacheSize / 1MB
-        
-        Write-Host "Profile $($profileDirectory.Name) - Cache Size: $($cacheSizeInMB)MB"
+    if (Test-Path $historyFile) {
+        $connectionString = "Data Source=$historyFile;Version=3;New=False;Compress=True;"
+        $connection = New-Object System.Data.SQLite.SQLiteConnection($connectionString)
+        $connection.Open()
+
+        $query = "SELECT url, title, last_visit_time FROM urls ORDER BY last_visit_time DESC"
+        $command = New-Object System.Data.SQLite.SQLiteCommand($query, $connection)
+        $reader = $command.ExecuteReader()
+
+        while ($reader.Read()) {
+            $url = $reader.GetValue(0)
+            $title = $reader.GetValue(1)
+            $lastVisitTime = [DateTime]::FromFileTimeUtc($reader.GetInt64(2))
+
+            Write-Output "URL: $url"
+            Write-Output "Title: $title"
+            Write-Output "Last Visit Time: $lastVisitTime"
+            Write-Output ""
+        }
+
+        $reader.Close()
+        $connection.Close()
+    }
+    else {
+        Write-Output "Chrome history file not found."
     }
 }

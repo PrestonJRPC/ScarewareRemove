3e77cf9223a2fc4376785202709dd9c555b9317b
Preston Callos <preston@jrpcinfosec.com>
2023-12-14 00:29:55 -0700

    first

--- Scareware_Removal.ps1
+++ Scareware_Removal.ps1
@@ -1,8 +1,24 @@
-$chromeProfilePath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
-$profileFolders = Get-ChildItem -Path $chromeProfilePath -Directory
+# Clear Chrome cache
+$chromeCachePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cache"
+Remove-Item -Path $chromeCachePath -Recurse -Force
 
-$profileNames = $profileFolders | ForEach-Object {
-    $_.Name
+# Clear Chrome browsing history
+$chromeHistoryPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History"
+Remove-Item -Path $chromeHistoryPath -Force
+
+# Turn off Chrome notifications
+$chromePreferencesPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Preferences"
+$chromePreferences = Get-Content -Path $chromePreferencesPath | ConvertFrom-Json
+$chromePreferences.profile.default_content_setting_values.notifications = 2
+$chromePreferences | ConvertTo-Json | Set-Content -Path $chromePreferencesPath
+
+# Turn off Chrome notifications in the registry
+$chromeRegistryPath = "HKCU:\Software\Policies\Google\Chrome"
+$chromeRegistryValueName = "DefaultNotificationsSetting"
+$chromeRegistryValueData = 2
+
+if (!(Test-Path $chromeRegistryPath)) {
+    New-Item -Path $chromeRegistryPath -Force | Out-Null
 }
 
-$profileNames
+Set-ItemProperty -Path $chromeRegistryPath -Name $chromeRegistryValueName -Value $chromeRegistryValueData
